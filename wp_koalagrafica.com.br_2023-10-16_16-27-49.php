1ba8ed7e2831d57d5c6dd477bdba1891bccf81d7
Wallan Ramos <129308887+wallanramos995@users.noreply.github.com>
2024-05-24 23:25:45 -0300

    Add files via upload

---
+++ wp_koalagrafica.com.br_2023-10-16_16-27-49.php
@@ -0,0 +1,15 @@
+<?php exit();?>
+{
+    "name": "wp_koalagrafica.com.br_2023-10-16_16-27-49",
+    "backup_dir": "1",
+    "backup_db": "1",
+    "email": "wallanramos95@gmail.com",
+    "date_time": "2023-10-16 16:28:pm",
+    "btime": 1697473694,
+    "auto_backup": false,
+    "ext": "tar.gz",
+    "size": false,
+    "backup_site_url": "https:\/\/koalagrafica.com.br",
+    "backup_site_path": "\/home\/u951072883\/domains\/koalagrafica.com.br\/public_html",
+    "backup_location": 1
+}
